1193520141dc296f6b88ec52630326eee5b2dc53
PanTester7 <80033919+Lokatex@users.noreply.github.com>
2025-02-26 10:18:40 +0100

    nie działa ten framework

---
+++ app-sidebar-tailwind/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import '../App.css';
+
+export default function Sidebar() {
+
+    const [open,setOpen] = useState(true)
+
+    return (
+        <nav className=' shadow-md h-screen w-60 bg-blue-600'>
+
+        </nav>
+    )
+}
